bda1b2c6d5ae25216cf2fd96793863771d74e9bc
Tomas Stolker <stolker@strw.leidenuniv.nl>
2024-04-30 10:36:08 +0200

    v0.0.4

--- calistar/__init__.py
+++ calistar/__init__.py
@@ -2,7 +2,7 @@ from calistar.calistar import CaliStar
 
 __author__ = "Tomas Stolker"
 __license__ = "MIT"
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 __maintainer__ = "Tomas Stolker"
 __email__ = "stolker@strw.leidenuniv.nl"
 __status__ = "Development"
